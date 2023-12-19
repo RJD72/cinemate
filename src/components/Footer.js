0797cc1f549e82ab4ea15f4a082a4091a1b182d2
robdeer1 <r_deer@fanshaweonline.ca>
2023-12-18 19:14:21 -0500

    Initial deployment

---
+++ src/components/Footer.js
@@ -0,0 +1,59 @@
+import { Link } from "react-router-dom";
+
+export const Footer = () => {
+  return (
+    <footer className="bg-white shadow dark:bg-gray-800">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2030
+          <Link to="/" className="hover:underline">
+            Cinemate
+          </Link>
+          . All Rights Reserved.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+          <li>
+            <a
+              href="https://www.instagram.com/"
+              target="_blank"
+              className="hover:underline me-4 md:me-6"
+              rel="noreferrer"
+            >
+              Instagram
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://ca.linkedin.com/"
+              target="_blank"
+              className="hover:underline me-4 md:me-6"
+              rel="noreferrer"
+            >
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.youtube.com/"
+              target="_blank"
+              className="hover:underline me-4 md:me-6"
+              rel="noreferrer"
+            >
+              YouTube
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/"
+              target="_blank"
+              className="hover:underline"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
